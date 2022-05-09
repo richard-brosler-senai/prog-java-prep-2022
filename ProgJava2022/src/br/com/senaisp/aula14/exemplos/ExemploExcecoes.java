217c23015d59186b69a6a7b9d365e9af950bb723
Richard Brosler <richard.brosler@sp.senai.br>
2022-05-08 23:20:02 -0300

    commit inicial

---
+++ ProgJava2022/src/br/com/senaisp/aula14/exemplos/ExemploExcecoes.java
@@ -0,0 +1,17 @@
+package br.com.senaisp.aula14.exemplos;
+
+import java.util.Scanner;
+
+public class ExemploExcecoes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Digite um número:");
+		int intNr = sc.nextInt();
+		System.out.println("Você digitou "+intNr);
+		sc.close();
+
+	}
+
+}
